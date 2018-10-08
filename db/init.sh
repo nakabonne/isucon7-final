ceab5bad9df1fd5e4c1fadfe75d1e73294ee0564
root <root@isucon7-final-1.c.isucon-210313.internal>
2018-10-08 06:06:54 +0000

    nakabonne first

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)
-mysql --default-character-set=utf8mb4 -uroot < $(pwd)/drop.sql
-mysql --default-character-set=utf8mb4 -uroot isudb < $(pwd)/isudb.sql
-mysql --default-character-set=utf8mb4 -uroot isudb < $(pwd)/m_item.sql
+mysql --default-character-set=utf8mb4 -uroot -pnakao < $(pwd)/drop.sql
+mysql --default-character-set=utf8mb4 -uroot -pnakao isudb < $(pwd)/isudb.sql
+mysql --default-character-set=utf8mb4 -uroot -pnakao isudb < $(pwd)/m_item.sql
---
+++ env.sh
@@ -0,0 +1,4 @@
+ISU_DB_HOST=localhost
+ISU_DB_PORT=3306
+ISU_DB_USER=root
+ISU_DB_PASSWORD=nakao
